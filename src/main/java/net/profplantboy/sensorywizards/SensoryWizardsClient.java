a9ef3a2f29254f0d543343e958b1e4a80dd7af89
profPlantBoy <nickolaskeefer@gmail.com>
2025-08-26 12:22:52 -0400

    Test of first real commit

---
+++ src/main/java/net/profplantboy/sensorywizards/SensoryWizardsClient.java
@@ -0,0 +1,11 @@
+package net.profplantboy.sensorywizards;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SensoryWizardsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
